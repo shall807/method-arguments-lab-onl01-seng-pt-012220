ece94eab68968d0d0c6a57afd6ec8d8dba76948d
Stevi Hall <career.sh.work@gmail.com>
2020-01-22 19:07:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
-def introduction_with_language(name_language)
+def introduction_with_language()
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
